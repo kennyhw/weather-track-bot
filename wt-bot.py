a83379517d137b78132e5b6eaa8f847c71ec6520
kennyhw <kennyhermawan7@gmail.com>
2020-07-13 19:30:58 +0800

    Add basic functionalities

--- wt-bot.py
+++ wt-bot.py
@@ -6,14 +6,16 @@ import requests
 from tk import get_token
 
 def main():
-    token = get_token()
-    updater = Updater(token)
+    access_token = get_token()
+    updater = Updater(token=access_token, use_context=True)
+    dispatcher = updater.dispatcher
 
-def command1():
-    data = get_data()
-    chat_id = update.message.chat_id
-    bot.send(chat_id=chat_id)
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+    updater.start_polling()
 
-def get_data():
-    data = {"text1":"Hello World"}
-    return data
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot. Beep boop.")
+
+if __name__ == '__main__':
+    main()
